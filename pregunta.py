56cc79dbdf0a2f2202f9b647e8a951edaef052d3
caroAM22 <caalvarezmu@unal.edu.co>
2024-03-08 12:32:50 -0500

    Update lab 4

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,38 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+
 import pandas as pd
 
 
 def clean_data():
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
+    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
 
-    #
-    # Inserte su código aquí
-    #
+    # Limpieza de monto_del_credito
+    df["monto_del_credito"] = (
+        df["monto_del_credito"]
+        .str.strip(" ")
+        .str.replace("[,$]|(\.00$)", "", regex=True)
+        .astype(float)
+    )
+    df["barrio"] = df["barrio"].str.lower().replace("[_-]", " ", regex=True)
+    # Limpieza de fecha_de_beneficio
+    df["fecha_de_beneficio"] = pd.to_datetime(
+        df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+    ).fillna(
+        pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")
+    )
+
+    # Limpieza de comuna_ciudadano
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    # Eliminación de duplicados y valores nulos
+    df = df.drop_duplicates().dropna()
 
     return df
+
+
+clean_data()
